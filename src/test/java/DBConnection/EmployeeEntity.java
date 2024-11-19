5fc5cd814f67de1f892ec657b8ca1cf7e47a0dcf
Pitoshi <xalidar@mail.ru>
2024-11-20 02:30:22 +0300

    Как запустить тесты

--- src/test/java/DBConnection/EmployeeEntity.java
+++ src/test/java/DBConnection/EmployeeEntity.java
@@ -77,10 +77,6 @@ public class EmployeeEntity {
         this.phone = phone;
     }
 
-    public String getEmail() {
-        return email;
-    }
-
     public void setEmail(String email) {
         this.email = email;
     }
@@ -89,16 +85,8 @@ public class EmployeeEntity {
         this.birthdate = birthdate;
     }
 
-    public String getAvatar_url() {
-        return avatar_url;
-    }
-
     public void setAvatar_url(String avatar_url) {
         this.avatar_url = avatar_url;
-    }
-
-    public int getCompanyId() {
-        return companyId;
     }
 
     public void setCompanyId(int companyId) {
